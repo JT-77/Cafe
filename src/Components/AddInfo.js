559b1e0420cb1603325b2e67a85abdf7fa1294c7
JT-77 <jatin7599@gmail.com>
2020-09-24 18:37:21 +0530

    filter view

--- src/Components/AddInfo.js
+++ src/Components/AddInfo.js
@@ -6,131 +6,146 @@ import Grid from '@material-ui/core/Grid';
 import fire from "./fire";
 
 class AddInfo extends Component {
-    state = {
-        PersonName: "",
-        TeamName: "",
-        Contact: null,
-        Location: "",
-        TableNumber: "",
-        filled: 1
-      };
-    
-      handleText = (event) => {
+  state = {
+    PersonName: "",
+    TeamName: "",
+    Contact: null,
+    Location: "",
+    TableNumber: "",
+    filled: 1,
+    match: ""
+  };
+
+  handleText = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  componentDidMount() {
+    fire
+      .database()
+      .ref("Matches").endAt().limitToLast(1).on("child_added", snap => {
+        console.log(snap.val())
+
+        var info = snap.val().match;
+
         this.setState({
-            [event.target.name]: event.target.value
-          });
-      };
-    
-      handleSubmit = e => {
-        
-        fire
-          .database()
-          .ref("Customers")
-          .child(this.state.Contact)
-          .set(this.state);
+          match: info
+        })
+      });
+  }
 
-        fire
-          .database()
-          .ref("Customers").endAt().limitToLast(1).once("child_added", function(snap) {
-            console.log("added:", snap.key);
+  handleSubmit = e => {
 
-            Swal.fire(
-              "Booking Created!",
-              "Unique ID = " + snap.key,
-              "info"
-            );
+    fire
+      .database()
+      .ref("Customers")
+      .child(this.state.Contact)
+      .set(this.state);
 
-          });  
-    
-        this.setState({
-          PersonName: "",
-          TeamName: "",
-          Contact: "",
-          Location: "",
-          TableNumber: "",
-          filled: 1
-        });
-      };
-
-      render() {
-
-        return(
-          <form>
-              <Grid container justify="center">
-                <Grid item xs={12} style={{ margin: '20px' }}>
-                  <TextField
-                    onChange={this.handleText}
-                    label="Customer Name"
-                    placeholder="Customer Name"
-                    fullWidth
-                    name="PersonName"
-                    size="small"
-                    variant="outlined"
-                    value={this.state.PersonName}
-                  />
-                </Grid>
-                <Grid item xs={12} style={{ margin: '20px' }}>
-                  <TextField
-                    onChange={this.handleText}
-                    label="Team Name"
-                    placeholder="Team Name"
-                    fullWidth
-                    name="TeamName"
-                    size="small"
-                    variant="outlined"
-                    value={this.state.TeamName}
-                  />
-                </Grid>
-                <Grid item xs={12} style={{ margin: '20px' }}>
-                  <TextField
-                    onChange={this.handleText}
-                    label="Contact"
-                    fullWidth
-                    placeholder="Contact"
-                    name="Contact"
-                    size="small"
-                    variant="outlined"
-                    value={this.state.Contact}
-                  />
-                </Grid>
-                <Grid item xs={12} style={{ margin: '20px' }}>
-                  <TextField
-                    onChange={this.handleText}
-                    label="Location"
-                    placeholder="Location"
-                    fullWidth
-                    name="Location"
-                    size="small"
-                    variant="outlined"
-                    value={this.state.Location}
-                  />
-                </Grid>
-                <Grid item xs={12} style={{ margin: '20px' }}>
-                  <TextField
-                    onChange={this.handleText}
-                    label="Table Number"
-                    placeholder="Table Number"
-                    fullWidth
-                    name="TableNumber"
-                    size="small"
-                    variant="outlined"
-                    value={this.state.TableNumber}
-                  />
-                </Grid>
-                <Grid item xs={12} style={{ margin: '20px' }}>
-                  <Button
-                    className="submit"
-                    variant="contained"
-                    color="secondary"
-                    onClick={this.handleSubmit}
-                  >
-                    Book Table
-                  </Button>
-                </Grid>
-              </Grid>
-            </form>
+    /*fire
+      .database()
+      .ref("Customers").endAt().limitToLast(1).once("child_added", function (snap) {
+        console.log("added:", snap.key);
+    */
+        Swal.fire(
+          "Booking Created!",
+          "Unique ID = " + this.state.Contact,
+          "info"
         );
-      }
+
+      //});
+
+    this.setState({
+      PersonName: "",
+      TeamName: "",
+      Contact: "",
+      Location: "",
+      TableNumber: "",
+      filled: 1
+    });
+  };
+
+  render() {
+
+    return (
+      <form>
+        <Grid container justify="center">
+          <Grid item xs={12} style={{ margin: '20px' }}>
+            <TextField
+              onChange={this.handleText}
+              label="Customer Name"
+              placeholder="Customer Name"
+              fullWidth
+              name="PersonName"
+              size="small"
+              variant="outlined"
+              value={this.state.PersonName}
+            />
+          </Grid>
+          <Grid item xs={12} style={{ margin: '20px' }}>
+            <TextField
+              onChange={this.handleText}
+              label="Team Name"
+              placeholder="Team Name"
+              fullWidth
+              name="TeamName"
+              size="small"
+              variant="outlined"
+              value={this.state.TeamName}
+            />
+          </Grid>
+          <Grid item xs={12} style={{ margin: '20px' }}>
+            <TextField
+              onChange={this.handleText}
+              label="Contact"
+              fullWidth
+              placeholder="Contact"
+              name="Contact"
+              size="small"
+              variant="outlined"
+              value={this.state.Contact}
+            />
+          </Grid>
+          <Grid item xs={12} style={{ margin: '20px' }}>
+            <TextField
+              onChange={this.handleText}
+              label="Location"
+              placeholder="Location"
+              fullWidth
+              name="Location"
+              size="small"
+              variant="outlined"
+              value={this.state.Location}
+            />
+          </Grid>
+          <Grid item xs={12} style={{ margin: '20px' }}>
+            <TextField
+              onChange={this.handleText}
+              label="Table Number"
+              placeholder="Table Number"
+              fullWidth
+              name="TableNumber"
+              size="small"
+              variant="outlined"
+              value={this.state.TableNumber}
+            />
+          </Grid>
+          <Grid item xs={12} style={{ margin: '20px' }}>
+            <Button
+              className="submit"
+              variant="contained"
+              color="secondary"
+              onClick={this.handleSubmit}
+            >
+              Book Table
+                  </Button>
+          </Grid>
+        </Grid>
+      </form>
+    );
+  }
 }
 
 export default AddInfo;
--- src/Components/ViewInfo.js
+++ src/Components/ViewInfo.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import fire from './fire';
+import TextField from "@material-ui/core/TextField";
+import Grid from '@material-ui/core/Grid';
 import '../App.css'
 
 class ViewInfo extends Component {
@@ -8,7 +10,8 @@ class ViewInfo extends Component {
         super(props);
 
         this.state = {
-            info: []
+            info: [],
+            filter: ""
         }
     }
 
@@ -23,53 +26,78 @@ class ViewInfo extends Component {
 
             this.setState({ info: list })
         })
+
+        fire
+            .database()
+            .ref("Matches").endAt().limitToLast(1).once("child_added", snap => {
+                var inf = snap.val().match;
+
+                this.setState({
+                    filter: inf
+                })
+            });
+    }
+
+    handleText = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    handleFilter = () => {
+        this.componentDidMount();
     }
 
     render() {
         return (
-            <div style={{ overflowX: 'auto', marginTop: '20px' }}>
-                <table>
-                    <thead class="thead-dark">
-                        <tr>
-                            <th>Name</th>
-                            <th>Table Number</th>
-                            <th>Location</th>
-                            <th>Contact</th>
-                            <th>Team Name</th>
-                            <th>Choice1</th>
-                            <th>Prediction1</th>
-                            <th>Choice2</th>
-                            <th>Prediction2</th>
-                            <th>Choice3</th>
-                            <th>Prediction3</th>
-                            <th>Choice4</th>
-                            <th>Prediction4</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.info.map(data => {
-
-                            return (
-                                <tr>
-                                    <td>{data.PersonName}</td>
-                                    <td>{data.TableNumber}</td>
-                                    <td>{data.Location}</td>
-                                    <td>{data.Contact}</td>
-                                    <td>{data.TeamName}</td>
-                                    <td>{data.choice1}</td>
-                                    <td>{data.prediction1}</td>
-                                    <td>{data.choice2}</td>
-                                    <td>{data.prediction2}</td>
-                                    <td>{data.choice3}</td>
-                                    <td>{data.prediction3}</td>
-                                    <td>{data.choice4}</td>
-                                    <td>{data.prediction4}</td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </div>
+            <Grid container>
+                <Grid item xs={12}>
+                    <TextField variant="outlined" name="filter" label="Enter Match Number" value={this.state.filter} style={{ float: 'right' }} onChange={this.handleText} />
+                </Grid>
+                <Grid item xs={12} style={{ overflowX: 'auto', marginTop: '20px' }}>
+                    <table>
+                        <thead class="thead-dark">
+                            <tr>
+                                <th>Name</th>
+                                <th>Table Number</th>
+                                <th>Location</th>
+                                <th>Contact</th>
+                                <th>Team Name</th>
+                                <th>Choice1</th>
+                                <th>Prediction1</th>
+                                <th>Choice2</th>
+                                <th>Prediction2</th>
+                                <th>Choice3</th>
+                                <th>Prediction3</th>
+                                <th>Choice4</th>
+                                <th>Prediction4</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.info.map(data => {
+                                if (this.state.filter == data.match)
+                                    return (
+                                        <tr>
+                                            <td>{data.PersonName}</td>
+                                            <td>{data.TableNumber}</td>
+                                            <td>{data.Location}</td>
+                                            <td>{data.Contact}</td>
+                                            <td>{data.TeamName}</td>
+                                            <td>{data.choice1}</td>
+                                            <td>{data.prediction1}</td>
+                                            <td>{data.choice2}</td>
+                                            <td>{data.prediction2}</td>
+                                            <td>{data.choice3}</td>
+                                            <td>{data.prediction3}</td>
+                                            <td>{data.choice4}</td>
+                                            <td>{data.prediction4}</td>
+                                        </tr>
+                                    );
+                            })}
+                        </tbody>
+                    </table>
+                </Grid>
+            </Grid>
         );
     }
 }
